596b35b70884a1b9bfe4a7b34a7ff412e68b2d74
Elybourland <JEBourland@yahoo.com>
2023-10-28 14:45:12 -0700

    adding a userslist page, component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    { 
+      id: 'u1', 
+      name: 'Jane Doe', 
+      image: 'https://images.pexels.com/photos/3408348/pexels-photo-3408348.jpeg?auto=compress&cs=tinysrgb&w=1600', 
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />
 };
 
 export default Users;
