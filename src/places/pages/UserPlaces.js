a0f2530714b05e89b335de97421fef63ab9d37aa
Elybourland <JEBourland@yahoo.com>
2023-11-09 19:47:08 -0800

    rendering user places

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most skyscrapers in the world!',
+    imageUrl: 'https://images.pexels.com/photos/2404949/pexels-photo-2404949.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    address: '20 W 34th St., New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      long: -73.9882447
+    },
+    creator: 'u1'  
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most skyscrapers in the world!',
+    imageUrl: 'https://images.pexels.com/photos/2404949/pexels-photo-2404949.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    address: '20 W 34th St., New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      long: -73.9882447
+    },
+    creator: 'u2'
+  }
+];
+
+const UserPlaces = () => {
+
+  return <PlaceList items={DUMMY_PLACES} />
+};
+
+export default UserPlaces;
